1c8c1266802c4d605a00af876e139aa5c307b4df
Rob March <robertmarch5@gmail.com>
2022-12-05 20:30:53 +0000

    Solve day 5 part b

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -6,46 +6,52 @@ pub struct Day05 {}
 
 impl Day for Day05 {
     fn solve_a(&self, file: &String) -> String {
-        let (initial, moves) = file.split_once("\n\n").unwrap();
-
-        let mut cargo_stacks: HashMap<usize, Vec<char>> = (1..10).map(|n| (n, vec![])).collect();
-
-        for line in initial.lines() {
-            if line.chars().nth(1).unwrap() == '1' {
-                break;
-            }
-
-            for (i, chr) in line.chars().enumerate() {
-                let column_number = (i + 3) / 4;
-                if (i + 3) % 4 == 0 && chr != ' ' {
-                    cargo_stacks.get_mut(&column_number).unwrap().splice(0..0, std::iter::once(chr));
-                }
-            }
-        }
-
-        for line in moves.lines() {
-            let parts: Vec<&str> = line.split(" ").collect();
-            let move_count: usize = parts[1].parse().expect("Unexpected number of times");
-            let source: usize = parts[3].parse().expect("Unexpected stack number");
-            let target: usize = parts[5].parse().expect("Unexpected stack number");
-
-            for _n in 1..move_count+1 {
-                let popped_val = cargo_stacks.get_mut(&source).unwrap().pop().unwrap();
-                cargo_stacks.get_mut(&target).unwrap().push(popped_val);
-
-            }
-
-        }
-        print_stacks(&cargo_stacks, 10);
-        
-        (1..10).map(|n| cargo_stacks.get_mut(&n).unwrap().pop().unwrap_or(' ')).collect()
+        solve(file, true)
     }
     
-    fn solve_b(&self, _file: &String) -> String {
-        String::from("Not yet implemented")
+    fn solve_b(&self, file: &String) -> String {
+        solve(file, false)
     }
 }
 
-fn print_stacks(cargo_stacks: &HashMap<usize, Vec<char>>, n_to_print: usize) {
-    (1..n_to_print+1).for_each(|n| println!("{:?}", cargo_stacks.get(&n).unwrap()));
+fn solve(file: &String, reverse_order_in_move: bool) -> String {
+    let (initial, moves) = file.split_once("\n\n").unwrap();
+
+    let mut cargo_stacks: HashMap<usize, Vec<char>> = (1..10).map(|n| (n, vec![])).collect();
+
+    for line in initial.lines() {
+        if line.chars().nth(1).unwrap() == '1' {
+            break;
+        }
+
+        for (i, chr) in line.chars().enumerate() {
+            let column_number = (i + 3) / 4;
+            if (i + 3) % 4 == 0 && chr != ' ' {
+                cargo_stacks.get_mut(&column_number).unwrap().push(chr);
+            }
+        }
+    }
+
+    for line in moves.lines() {
+        let parts: Vec<&str> = line.split(" ").collect();
+        let move_count: usize = parts[1].parse().expect("Unexpected number of times");
+        let source: usize = parts[3].parse().expect("Unexpected stack number");
+        let target: usize = parts[5].parse().expect("Unexpected stack number");
+
+        let mut removed_elements: Vec<char> = cargo_stacks.get_mut(&source).unwrap().splice(0..move_count, std::iter::empty()).collect();
+
+        if reverse_order_in_move {
+            removed_elements.reverse();
+        }
+        
+        cargo_stacks.get_mut(&target).unwrap().splice(0..0, removed_elements);
+    }
+    
+    (1..10).map(|n| {
+        let stack = cargo_stacks.get_mut(&n).unwrap();
+        if stack.len() > 0 {
+            return stack.splice(0..1, std::iter::empty()).collect::<String>();
+        }
+        String::from(' ')
+    }).collect()
 }
